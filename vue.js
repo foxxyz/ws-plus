f874c355593735f7a4b7b731914306812b0fe92b
foxxyz <foxxyz@gmail.com>
2021-06-20 20:29:37 -0700

    fix: allow createSocket calls to set autoConnect: false if desired, simplify

--- vue.js
+++ vue.js
@@ -2,12 +2,10 @@ import { Client } from '.'
 import { inject, onMounted, onUnmounted, reactive } from 'vue'
 
 export function createSocket(url, options) {
-    const socket = reactive(new Client(url, { autoConnect: false, ...options }))
-    socket.connect()
-    return socket
+    return reactive(new Client(url, options))
 }
 
-export function listen(actions, { name='ws'}={}) {
+export function listen(actions, { name='ws' }={}) {
     const client = inject(`$${name}`)
     onMounted(() => {
         for(const action in actions) {
