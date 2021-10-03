c23c2897138b91f6ab57ffd88c29fc253009ef33
foxxyz <foxxyz@gmail.com>
2021-10-03 15:13:50 -0400

    fix(vue-plugin): reactivity of connected attribute, close #19

---
+++ test/vue-plugin-behavior.test.js
@@ -0,0 +1,21 @@
+import { watch } from 'vue'
+
+import { createSocket } from '../vue'
+import { MockSocket } from './__mocks__/socket'
+
+global.WebSocket = MockSocket
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms))
+
+describe('Vue 3 Plugin', () => {
+    describe('Behavior', () => {
+        it('makes the connected attribute reactive', async() => {
+            const client = createSocket('ws://localhost:8888')
+            const listener = jest.fn()
+            watch(() => client.connected, listener)
+            await delay(100)
+            expect(client.connected).toBe(true)
+            expect(listener).toHaveBeenCalledWith(true, false, expect.anything())
+        })
+    })
+})
--- vue.js
+++ vue.js
@@ -1,8 +1,14 @@
 import { Client } from '.'
 import { inject, onMounted, onUnmounted, reactive } from 'vue'
 
-export function createSocket(url, options) {
-    return reactive(new Client(url, options))
+export function createSocket(url, { autoConnect=true, ...options } = {}) {
+    const socket = reactive(new Client(url, { autoConnect: false, ...options }))
+    // The reason automatic connection is handled differently here,
+    // is because calling `this.connect()` in the constructor pre-empts
+    // the reactivity of the object and makes users unable to watch the `connected`
+    // attribute; so instead we run `connect()` on the reactive client
+    if (autoConnect) socket.connect()
+    return socket
 }
 
 export function listen(actions, { name='ws' }={}) {
