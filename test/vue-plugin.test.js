cd910c0568a49e3c2b358939a125292bf75f6a0a
foxxyz <foxxyz@gmail.com>
2023-12-31 14:57:15 -0800

    feat!: remove vue2 support

--- test/vue-plugin.test.js
+++ test/vue-plugin.test.js
@@ -1,6 +1,8 @@
 import { jest } from '@jest/globals'
 import { JSDOM } from 'jsdom'
 
+import { MockSocket } from './mocks/socket'
+
 // Set up DOM globals before importing any modules that need it
 const dom = new JSDOM('<body><div id="app" /></body>')
 global.window = dom.window
@@ -8,33 +10,13 @@ global.Element = dom.window.Element
 global.SVGElement = dom.window.SVGElement
 global.document = dom.window.document
 global.WebSocket = dom.window.WebSocket
-const { createApp } = await import('vue')
-const { Client, Server } = await import('..')
+const { createApp, watch } = await import('vue')
+const { Server } = await import('..')
 const { createSocket, listen } = await import('../vue.js')
 
 const delay = ms => new Promise(res => setTimeout(res, ms))
 
-describe('Vue 2 Plugin', () => {
-    let MockFramework
-    beforeEach(() => {
-        MockFramework = class {}
-        MockFramework.util = { defineReactive: () => {} }
-    })
-    it('can be installed', () => {
-        const client = new Client('ws://localhost:8888', { autoConnect: false, verbosity: 0 })
-        client.install(MockFramework)
-        const app = new MockFramework()
-        expect(app.$ws).toBe(client)
-    })
-    it('allows overriding global reference name', () => {
-        const client = new Client('ws://localhost:8888', { autoConnect: false, verbosity: 0 })
-        client.install(MockFramework, { name: 'anotherSocket' })
-        const app = new MockFramework()
-        expect(app.$anotherSocket).toBe(client)
-    })
-})
-
-describe('Vue 3 Plugin', () => {
+describe('Vue Plugin', () => {
     let server, app
     afterEach(async() => {
         app.unmount()
@@ -138,5 +120,17 @@ describe('Vue 3 Plugin', () => {
         client.close()
         client2.close()
         await Promise.all([closing, closing2])
+    })
+})
+
+describe('Vue Plugin Behavior', () => {
+    it('makes the connected attribute reactive', async() => {
+        global.WebSocket = MockSocket
+        const client = createSocket('ws://localhost:8888')
+        const listener = jest.fn()
+        watch(() => client.connected, listener)
+        await delay(100)
+        expect(client.connected).toBe(true)
+        expect(listener).toHaveBeenCalledWith(true, false, expect.anything())
     })
 })
